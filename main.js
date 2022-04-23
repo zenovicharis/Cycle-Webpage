effc96c1574b871492357327ebd8fbc2d084c4c6
Zenovic Haris <zenovicharis@live.com>
2022-04-23 23:34:20 +0200

    finalized elements

--- main.js
+++ main.js
@@ -5,62 +5,71 @@ import Swiper from "swiper";
 import Gumshoe from "gumshoejs";
 import SmoothScroll from "smooth-scroll";
 
-var menu = document.getElementById("menu-toggle");
-var header = document.getElementsByTagName("header")[0];
-var menuBack = document.getElementById("menu-toggle-back");
-var mobileMenuButtons = document.querySelectorAll(".mobile-menu-inner a");
-var menuContent = document.getElementById("menu-content");
-var questions = document.querySelectorAll(".question-answer");
+const header = document.getElementsByTagName("header")[0];
+const questions = document.querySelectorAll(".question-answer");
+const logoLoader = document.querySelector(".logo-container");
+const loader = document.querySelector("section.loading");
+const wrapper = document.querySelector(".wrapper");
 
-var spy = new Gumshoe("#menu-list-desktop a", {
-  offset: function () {
-    return header.getBoundingClientRect().height;
-  },
-  events: true,
-});
-
-document.addEventListener(
-  "gumshoeActivate",
-  function (event) {
-    if (event.detail.content.id === "sponsors") {
-      header.classList.add("colored");
-    }
-
-    if (event.detail.content.id === "main") {
-      header.classList.remove("colored");
-    }
-  },
-  false
-);
-
-new SmoothScroll('a[href*="#"]', {
-  header: "header",
-});
-
-mobileMenuButtons.forEach((link) => {
-  link.addEventListener("click", () => {
-    menuContent.classList.remove("display");
+const mainFunction = () => {
+  logoLoader.addEventListener("click", function () {
+    this.classList.toggle("loaded");
   });
-});
 
-menu.addEventListener("click", () => {
-  menuContent.classList.toggle("display");
-});
-
-menuBack.addEventListener("click", () => {
-  menuContent.classList.toggle("display");
-});
-
-questions.forEach((item) => {
-  item.addEventListener("click", function (e) {
-    e.preventDefault();
-    let radioButton = this.querySelector("input");
-    radioButton.checked = !radioButton.checked;
+  var spy = new Gumshoe("#menu-list-desktop a", {
+    offset: function () {
+      return header.getBoundingClientRect().height;
+    },
+    events: true,
+    reflow: true,
   });
-});
 
-const swiper = new Swiper(".card-swiper", {
-  slidesPerView: "auto",
-  centeredSlides: true,
-  spaceBetween: 10,
+  document.addEventListener("gumshoeActivate", headerSpyFunction, false);
+
+  new SmoothScroll('a[href*="#"]', {
+    header: "header",
+  });
+
+  questions.forEach((item) => {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      let radioButton = this.querySelector("input");
+      radioButton.checked = !radioButton.checked;
+    });
+  });
+
+  const swiper = new Swiper(".card-swiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 10,
+  });
+};
+
+const headerSpyFunction = function (event) {
+  if (event.detail && event.detail.content.id === "main") {
+    header.classList.remove("colored");
+  } else {
+    header.classList.add("colored");
+  }
+};
+
+const scrollIntoSection = () => {
+  let urlElements = window.location.href.split("#");
+  let section = urlElements[1];
+  if (section) {
+    document.getElementById(urlElements[1]).scrollIntoView();
+    if (section != "main") {
+      headerSpyFunction({});
+    }
+  }
+};
+
+window.addEventListener("load", () => {
+  mainFunction();
+  logoLoader.classList.add("loaded");
+  setTimeout(() => {
+    wrapper.classList.remove("hidden");
+    loader.classList.add("hidden");
+    scrollIntoSection();
+  }, 1000);
 });
